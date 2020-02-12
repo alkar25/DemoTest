be9389f94c3c5500acfd15d82ced5d6c9c3d0931
alkar25 <60907832+alkar25@users.noreply.github.com>
2020-02-12 17:31:24 -0500

    Update emo12.java

--- demotest1/src/demotest1/emo12.java
+++ demotest1/src/demotest1/emo12.java
@@ -9,6 +9,7 @@ System.out.println("hello");
 System.out.println("heelo");
 System.out.println("heelo3");
 		System.out.println("hello online");
+		System.out.println("hello online2");
 		
 //>>>>>>> branch 'master' of https://github.com/alkar25/DemoTest.git
 	}
